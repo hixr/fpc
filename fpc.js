2826a36a331bebe7371605576732bab1139163db
hixr <vasil.libedenets@gmail.com>
2015-07-27 23:39:35 +0300

    tested

--- fpc.js
+++ fpc.js
@@ -20,7 +20,46 @@
     }
     function runMe() {
         var plotFrame = $('#fooplot-frame');
-        $('#plots_add ~ div').slideUp();
+        var plotStyle = {
+            width: "1800px"
+            , height: "700px"
+        }
+        var funcStyle = {
+            width: ""
+            , display: "block"
+        }
+        var leftStyle = {
+            display: "inline-block"
+            , float: "left"
+            , marginTop: ""
+            , padding: ""
+        }
+        var rightStyle = {
+            display: "inline-block"
+            , paddingLeft: ""
+            , width: ""
+        }
+        var slide = 'table:first'
+            + ', #plots_add ~ div'
+            + ', #dialog-permalink + div'
+            + ', tr:nth-child(3):last'
+            + ', tr:nth-child(4) td:has(table:only-child)'
+            + ', td:first-child>b:only-child'
+            + ', td[valign!="top"]:contains("... link to this page:")'
+            + ', table:nth-of-type(2) tr:last'
+            + ', div:has(table[cellspacing="0"])';
+        $(slide).slideUp();
+        $('td>button:has(span:contains("Permalink"))')
+            .closest('td')
+            .appendTo('table[width="100%"]>tbody>tr:first-child');
+        $('input.data-eq.equation').css("width", "97%");
+        $('li.plot.ui-widget-content').css("width", "300%");
+        $('#plots_add_type-button').css(funcStyle)
+            .closest('table').css("width", "100%");
+        plotFrame.css(plotStyle);
+        plotFrame.siblings().last().css(leftStyle);
+        plotFrame.parent().next().appendTo(plotFrame.parent()).css(rightStyle);
     }
-    window.onload = runMe;
+    $('body').click(runMe);
 }());
+
