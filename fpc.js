80aa8e38d4c38373a5a3f78bccd01c9e71aa69be
hixr <vasil.libedenets@gmail.com>
2015-07-27 18:49:56 +0300

    test

---
+++ fpc.js
@@ -0,0 +1,25 @@
+// ==UserScript==
+// @version     0.0.1
+// @name        Force playlist autoplay for YouTube
+// @match       *://fooplot.com/*
+// @run-at      document-start
+// @grant       none
+// @noframes
+// ==/UserScript==
+(function fpc() {
+    'use strict';
+    var injection;
+    if (document.querySelector('[name="html5player/html5player"]')) {
+        window.location.reload(false);
+    } else if (!document.getElementById('fpc')) {
+        injection = document.createElement('script');
+        injection.id = 'fpc';
+        injection.textContent = '(' + fpc + '())';
+        document.documentElement.appendChild(injection);
+        return;
+    }
+    function runMe() {
+        alert('ok');
+    }
+    window.onload = runMe;
+}());
